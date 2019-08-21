f7d6ce15ef708c6aacac851a9d035e7f277ce411
meihuanyu <763084024@qq.com>
2019-08-21 18:38:25 +0800

    :bug: fix

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useGlobalState, connect} from '../lib/index';
+import { useGlobalState, connect} from '../index';
 import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
